72984b70ec144f2bdb00d9c00e460dca0ebc85c0
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-03-31 15:38:09 -0700

    add other concepts for the flashcards

--- js/FCscripts.js
+++ js/FCscripts.js
@@ -1,6 +1,34 @@
 $(document).ready(function() {
-  $("p").click(function() {
+  $("p#javascript").click(function() {
     $("#javascript-showing").toggle();
     $("#javascript-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#operators").click(function() {
+    $("#operators-showing").toggle();
+    $("#operators-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#variables").click(function() {
+    $("#variables-showing").toggle();
+    $("#variables-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#vnc").click(function() {
+    $("#vnc-showing").toggle();
+    $("#vnc-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#functions").click(function() {
+    $("#functions-showing").toggle();
+    $("#functions-hidden").toggle();
   });
 });
