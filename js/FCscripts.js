b8b4b4cbc622100d01cbf6a81223e20011c31da1
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-04-02 11:13:45 -0700

    add the rest of the key words, and fix the respective css and js files to make the flashcards work

--- js/FCscripts.js
+++ js/FCscripts.js
@@ -32,3 +32,101 @@ $(document).ready(function() {
     $("#functions-hidden").toggle();
   });
 });
+
+$(document).ready(function() {
+  $("p#methods").click(function() {
+    $("#methods-showing").toggle();
+    $("#methods-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#arguments").click(function() {
+    $("#arguments-showing").toggle();
+    $("#arguments-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#parameters").click(function() {
+    $("#parameters-showing").toggle();
+    $("#parameters-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#return").click(function() {
+    $("#return-showing").toggle();
+    $("#return-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#cmethods").click(function() {
+    $("#cmethods-showing").toggle();
+    $("#cmethods-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#strngs").click(function() {
+    $("#strngs-showing").toggle();
+    $("#strngs-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#boo").click(function() {
+    $("#boo-showing").toggle();
+    $("#boo-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#undef").click(function() {
+    $("#undef-showing").toggle();
+    $("#undef-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#nan").click(function() {
+    $("#nan-showing").toggle();
+    $("#nan-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#null").click(function() {
+    $("#null-showing").toggle();
+    $("#null-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#alrt").click(function() {
+    $("#alrt-showing").toggle();
+    $("#alrt-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#comm").click(function() {
+    $("#comm-showing").toggle();
+    $("#comm-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#jq").click(function() {
+    $("#jq-showing").toggle();
+    $("#jq-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $("p#att").click(function() {
+    $("#att-showing").toggle();
+    $("#att-hidden").toggle();
+  });
+});
