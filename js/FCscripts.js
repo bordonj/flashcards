70afa6161734976313adfac97baadbf7b6d84546
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-04-03 14:11:53 -0700

    clean the FCscripts.js file

--- js/FCscripts.js
+++ js/FCscripts.js
@@ -3,128 +3,92 @@ $(document).ready(function() {
     $("#javascript-showing").toggle();
     $("#javascript-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#operators").click(function() {
     $("#operators-showing").toggle();
     $("#operators-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#variables").click(function() {
     $("#variables-showing").toggle();
     $("#variables-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#vnc").click(function() {
     $("#vnc-showing").toggle();
     $("#vnc-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#functions").click(function() {
     $("#functions-showing").toggle();
     $("#functions-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#methods").click(function() {
     $("#methods-showing").toggle();
     $("#methods-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#arguments").click(function() {
     $("#arguments-showing").toggle();
     $("#arguments-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#parameters").click(function() {
     $("#parameters-showing").toggle();
     $("#parameters-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#return").click(function() {
     $("#return-showing").toggle();
     $("#return-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#cmethods").click(function() {
     $("#cmethods-showing").toggle();
     $("#cmethods-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#strngs").click(function() {
     $("#strngs-showing").toggle();
     $("#strngs-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#boo").click(function() {
     $("#boo-showing").toggle();
     $("#boo-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#undef").click(function() {
     $("#undef-showing").toggle();
     $("#undef-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#nan").click(function() {
     $("#nan-showing").toggle();
     $("#nan-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#null").click(function() {
     $("#null-showing").toggle();
     $("#null-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#alrt").click(function() {
     $("#alrt-showing").toggle();
     $("#alrt-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#comm").click(function() {
     $("#comm-showing").toggle();
     $("#comm-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#jq").click(function() {
     $("#jq-showing").toggle();
     $("#jq-hidden").toggle();
   });
-});
 
-$(document).ready(function() {
   $("p#att").click(function() {
     $("#att-showing").toggle();
     $("#att-hidden").toggle();
